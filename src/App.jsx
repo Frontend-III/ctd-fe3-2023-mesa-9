4602d72f93a839cf63aec8f4c325b7518366bbf2
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2023-03-02 22:27:22 -0300

    Mesa 9

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,16 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Média de Alunos por Disciplina</h1>
+      <h1>Promedio de estudiantes por carrera</h1>
       <form className="form" onSubmit={handleSubmit}>
         <div className="container_input">
           <select>
             <option selected disabled>
-              Selecione uma disciplina
+              Selecione una carrera
             </option>
-            <option value="database">Banco de Dados</option>
-            <option value="backend">Desenvolvimento Backend</option>
-            <option value="frontend">Desenvolvimento Frontend</option>
+            <option value="database">Base de Datos</option>
+            <option value="backend">Desarrollo Backend</option>
+            <option value="frontend">Desarrollo Frontend</option>
             <option value="devops">Devops</option>
           </select>
           <input />
@@ -28,26 +28,26 @@ function App() {
         <table border="1" className="line_title">
           <thead>
             <tr>
-              <th>Disciplina</th>
-              <th>Quantidade de Alunos</th>
-              <th>Média das notas dos alunos</th>
+              <th>Carrera</th>
+              <th>Cantidad de Estudiantes</th>
+              <th>Promedio de Calificaciones de los Estudiantes</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>Banco de Dados</td>
+              <td>Base de Datos</td>
               <td>0</td>
               <td>0</td>
             </tr>
 
             <tr>
-              <td>Desenvolvimento Frontend</td>
+              <td>Desarrollo Frontend</td>
               <td>0</td>
               <td>0</td>
             </tr>
 
             <tr>
-              <td>Desenolvimento Backend</td>
+              <td>Desarrollo Backend</td>
               <td>0</td>
               <td>0</td>
             </tr>
